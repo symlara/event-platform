f660ebcfae58f2f97059ab7a071c03fbccd3e202
aaliyahsymlar <asym14@gmail.com>
2024-03-12 15:43:35 -0500

    implement stripe

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,17 @@
+import Collection from "@/components/shared/Collection";
 import { Button } from "@/components/ui/button";
+import { getAllEvents } from "@/lib/actions/event.actions";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const events = await getAllEvents({
+    query: "",
+    category: "",
+    page: 1,
+    limit: 6,
+  });
+
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
@@ -39,9 +48,18 @@ export default function Home() {
         </h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
-          Search 
-          Category Filter
+          Search Category Filter
         </div>
+
+        <Collection
+          data={events?.data}
+          emptyTitle="No Event found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
+          limit={6}
+          page={1}
+          totalPages={2}
+        />
       </section>
     </>
   );
--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -9,11 +9,11 @@ export interface IEvent extends Document {
   imageUrl: string;
   startDateTime: Date;
   endDateTime: Date;
-  price?: string;
+  price: string;
   isFree: boolean;
   url?: string;
-  category?: { _id: string; name: string }; // Assuming 'Category' is the name of the Category model
-  organizer?: { _id: string; firstName: string; lastName: string }; // Assuming 'User' is the name of the User model
+  category: { _id: string; name: string };
+  organizer: { _id: string; firstName: string; lastName: string };
 }
 
 const EventSchema = new Schema({
